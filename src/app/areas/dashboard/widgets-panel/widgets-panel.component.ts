b82d4c448d280eb6c99a1fce9e356b91cedf0994
Maciej Lipiński <maciej@statlook.com>
2024-05-24 13:13:58 +0200

    angular update and formatting

--- src/app/areas/dashboard/widgets-panel/widgets-panel.component.ts
+++ src/app/areas/dashboard/widgets-panel/widgets-panel.component.ts
@@ -15,14 +15,16 @@ export class WidgetsPanelComponent implements OnInit {
     Nie używaj klas Tilewind jako atrybutów, a tylko jako nazwy klas.
     Uważaj na zduplikowanie class="..." w tym samym elemencie html. Wszystkie klasy dla danego elementu powinny być w jednym class="...".
     Oto główne zasady modyfikacji:
-    1) fxFlex="row" lub 'layout-row' zamiana na 'flex-row flex flex-none'. Uwaga: jeśli element z fxFlex="row" posiada równocześnie fxFlex lub 'flex' w klasach, to bez 'flex-none', czyli samo: 'flex-row flex'.
-    2) fxFlex="column" lub 'layout-column' zamiana analogicznie jak fxFlex="row".
+    1) fxFlex="row" lub 'layout-row' zamiana na 'flex-row flex flex-none'.
+    Uwaga bardzo ważne: jeśli element z fxFlex="row" posiada równocześnie jeszcze dodatkowo fxFlex lub 'flex' w klasach, to zamiana bez 'flex-none', czyli na klasy: 'flex-row flex'.
+    2) fxFlex="column" lub 'layout-col' zamiana na 'flex-col flex flex-none'.
+    Uwaga bardzo ważne: jeśli element z fxFlex="column" posiada równocześnie jeszcze dodatkowo fxFlex lub 'flex' w klasach, to zamiana bez 'flex-none', czyli na klasy: 'flex-col flex'.
     3) fxFlex="XX" lub 'flex-XX', gdzie XX oznacza liczbę procentową, zamiana na 'basis-[XX%]', czyli np. fxFlex="70" na 'basis-[70%]'.
     4) Samo fxFlex lub 'flex' (bez fxFlex="row" i bez fxFlex="column", zamiana na 'flex-1' lub odpowiednią klasę basis, np. 'basis-0' lub 'basis-auto', w zależności od kontekstu.
     5) fxFlex="{{ myVar }}" lub 'flex-{{myVar}}', gdzie myVar to dowolna nazwa zmiennej, zamiana na: 'basis-['myVar'%]'.
     6) fxLayoutAlign="start center" lub 'layout-align-start-center' zamiana na 'justify-start items-center' dla 'flex-row'. Dla 'flex-col' justify i items są zamienione wartościami.
     7) fxLayoutFill lub 'layout-fill' zamiana na: 'w-full h-full'.
-    8) Przykładowe zamiany:
+    8) Przykłady zmian:
     a)
     <div class="content-text-container" fxLayout="row" fxLayoutAlign="start center">
     zamiana na:
